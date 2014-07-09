8d6328b6dcac88eec2a062b4d13e696a01d69eef
avocadoccme <yiransh@gmail.com>
2014-07-09 02:06:09 -0700

    rename

--- reverseWords.java
+++ reverseWords.java
@@ -1,11 +1,13 @@
-public String reverseWords(String s) {
-	if (s == null || s.isEmpty()) return "";
-	String[] arr = s.split(" ");
-	StringBuilder sb = new StringBuilder();
-	for(int i = arr.length-1; i>=0; i--){
-		if(!arr[i].equals("")){
-			sb.append(arr[i]).append(" ");	
+public class ReverseWords{
+	public String reverseWords(String s) {
+		if (s == null || s.isEmpty()) return "";
+		String[] arr = s.split(" ");
+		StringBuilder sb = new StringBuilder();
+		for(int i = arr.length-1; i>=0; i--){
+			if(!arr[i].equals("")){
+				sb.append(arr[i]).append(" ");	
+			}
 		}
+		return sb.length()==0? "" : sb.substring(0, sb.length()-1);
 	}
-	return sb.length()==0? "" : sb.substring(0, sb.length()-1);
 }
--- twoSum.java
+++ twoSum.java
@@ -1,15 +1,17 @@
-public int[] twoSum(int[] numbers, int target){
-	HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
-	int[] res = new int[2];
-	for(int i=0; i<numbers.length; i++){
-		int tt = target - numbers[i];
-		if(hash.containsKey(tt)){
-			res[0] = i+1;
-			res[1] = hash.get(tt);
-			Arrays.sort(res);
-		}else{
-			hash.put(numbers[i], i+1);
+public class TwoSum{
+	public int[] twoSum(int[] numbers, int target){
+		HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+		int[] res = new int[2];
+		for(int i=0; i<numbers.length; i++){
+			int tt = target - numbers[i];
+			if(hash.containsKey(tt)){
+				res[0] = i+1;
+				res[1] = hash.get(tt);
+				Arrays.sort(res);
+			}else{
+				hash.put(numbers[i], i+1);
+			}
 		}
+		return res;
 	}
-	return res;
 }
